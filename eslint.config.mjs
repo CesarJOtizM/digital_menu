10d4da8b7f3f4500e9f28707f6009ac97cd37c3e
Cesar Ortiz <cesarjavierortizmontero@gmail.com>
2026-06-24 11:54:05 -0500

    chore: scaffold single-tenant digital-menu toolchain and config

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import nextConfig from "eslint-config-next";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+/** @type {import("eslint").Linter.Config[]} */
+const eslintConfig = [
+  ...nextConfig,
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  {
+    ignores: ["node_modules/", ".next/", "src/prisma/generated/"],
+  },
+];
+
+export default eslintConfig;
